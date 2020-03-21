7831ea9fcfcb4a5f3c0633acb15cb9612e2151ed
Benjamin Eskildsen <benjamin.eskildsen@yale.edu>
2020-03-20 23:47:21 -0700

    Hanging Gardens

--- bin/bundle.js
+++ bin/bundle.js
@@ -1,6 +1,8 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 'use strict';
 
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
 
 var React = require('react');
@@ -10,7 +12,36 @@ var useState = React.useState,
     useMemo = React.useMemo;
 
 
-var config = {
+var greenConfig = {
+  gridWidth: 60,
+  gridHeight: 85,
+  msPerTick: 100,
+  liveMin: 2, // if alive, must have at least this many live neighbors to stay alive
+  liveMax: 5, // if alive, must have at most this many live neighbors to stay alive
+  deadMin: 3, // if dead, must have at least this many live neighbors to become alive
+  deadMax: 3, // if dead, must have at most this many live neighbors to become alive
+  noiseRate: 0.2,
+  backgroundColor: 'black',
+  cellColor: '#6B8E23',
+  decayRate: 0.06,
+  onClick: function onClick(ev) {
+    var cellWidth = width / config.gridWidth;
+    var cellHeight = height / config.gridHeight;
+    var gridX = Math.floor(ev.clientX / cellWidth);
+    var gridY = Math.floor(ev.clientY / cellHeight);
+    grid[gridX][gridY] = 1;
+  },
+  noiseFn: function noiseFn(x, y, width, height, noiseRate, nextAlive) {
+    if (y == 0 && Math.random() < noiseRate
+    // y == 2 && Math.random() < noiseRate / 2
+    ) {
+        return 1;
+      }
+    return nextAlive;
+  }
+};
+
+var redConfig = {
   gridWidth: 50,
   gridHeight: 70,
   msPerTick: 100,
@@ -20,35 +51,61 @@ var config = {
   deadMax: 3, // if dead, must have at most this many live neighbors to become alive
   noiseRate: 0.4,
   backgroundColor: 'black',
-  decayRate: 0.01
+  cellColor: 'red',
+  decayRate: 0.01,
+  onClick: function onClick(ev) {
+    var cellWidth = width / config.gridWidth;
+    var cellHeight = height / config.gridHeight;
+    var gridX = Math.floor(ev.clientX / cellWidth);
+    var gridY = Math.floor(ev.clientY / cellHeight);
+    grid[gridX][gridY] = 1;
+  },
+  noiseFn: function noiseFn(x, y, width, height, noiseRate, nextAlive) {
+    if (y == height - 1 && Math.random() < noiseRate || y == height - 2 && Math.random() < noiseRate / 2) {
+      return 1;
+    }
+    return nextAlive;
+  }
 };
 
-function App() {
+function App(props) {
+  // const {config} = props;
   var _document$getElementB = document.getElementById('container').getBoundingClientRect(),
       width = _document$getElementB.width,
       height = _document$getElementB.height;
 
-  var _useState = useState(initGrid(config.gridWidth, config.gridHeight)),
+  var _useState = useState(function () {
+    return _extends({}, props.config, {
+      gridWidth: width > height ? props.config.gridHeight : props.config.gridWidth,
+      gridHeight: width > height ? props.config.gridWidth : props.config.gridHeight
+    });
+  }),
       _useState2 = _slicedToArray(_useState, 2),
-      grid = _useState2[0],
-      setGrid = _useState2[1];
+      config = _useState2[0],
+      setConfig = _useState2[1];
+
+  var _useState3 = useState(initGrid(config.gridWidth, config.gridHeight)),
+      _useState4 = _slicedToArray(_useState3, 2),
+      grid = _useState4[0],
+      setGrid = _useState4[1];
 
   useEffect(function () {
     var interval = setInterval(function () {
       setGrid(function (grid) {
-        return advanceGrid(grid);
+        return advanceGrid(grid, config);
       });
     }, config.msPerTick);
     return function () {
       return clearInterval(interval);
     };
-  }, []);
+  }, [config.gridWidth]);
 
   var cells = [];
   for (var x = 0; x < config.gridWidth; x++) {
     for (var y = 0; y < config.gridHeight; y++) {
       cells.push(React.createElement(Cell, {
         key: 'cell_' + x + '_' + y,
+        config: config,
         x: x, y: y, alive: grid[x][y]
       }));
     }
@@ -63,13 +120,7 @@ function App() {
         backgroundColor: config.backgroundColor,
         position: 'relative'
       },
-      onClick: function onClick(ev) {
-        var cellWidth = width / config.gridWidth;
-        var cellHeight = height / config.gridHeight;
-        var gridX = Math.floor(ev.clientX / cellWidth);
-        var gridY = Math.floor(ev.clientY / cellHeight);
-        grid[gridX][gridY] = 1;
-      }
+      onClick: config.onClick
     },
     cells
   );
@@ -78,34 +129,43 @@ function App() {
 function Cell(props) {
   var x = props.x,
       y = props.y,
-      alive = props.alive;
+      alive = props.alive,
+      config = props.config;
 
   var _document$getElementB2 = document.getElementById('container').getBoundingClientRect(),
       width = _document$getElementB2.width,
       height = _document$getElementB2.height;
 
   var gridWidth = config.gridWidth,
-      gridHeight = config.gridHeight;
+      gridHeight = config.gridHeight,
+      cellColor = config.cellColor;
 
 
   var cellWidth = width / gridWidth;
   var cellHeight = height / gridHeight;
-  var backgroundColor = 'rgba(255, 0, 0, ' + alive + ')';
+  // const backgroundColor = 'rgba(255, 0, 0, ' + alive + ')';
+  var color = cellColor;
+  if (alive > 50) {
+    color = "#D2691E";
+  }
   return React.createElement('div', {
     style: {
       position: 'absolute',
-      backgroundColor: backgroundColor,
+      backgroundColor: color,
       top: cellHeight * y,
       left: cellWidth * x,
       width: cellWidth,
-      height: cellHeight
+      height: cellHeight,
+      opacity: alive
     }
   });
 }
 
-function advanceGrid(grid) {
-  var width = grid.length;
-  var height = grid[0].length;
+function advanceGrid(grid, config) {
+  // const width = grid.length;
+  // const height = grid[0].length;
+  var width = config.gridWidth;
+  var height = config.gridHeight;
   var nextGrid = [];
   for (var x = 0; x < width; x++) {
     var nextCol = [];
@@ -113,17 +173,15 @@ function advanceGrid(grid) {
       var alive = grid[x][y];
       var numAliveNeighbors = getNumAliveNeighbors(grid, x, y);
       var nextAlive = alive;
-      if (alive == 1) {
-        nextAlive = numAliveNeighbors < config.liveMin || numAliveNeighbors > config.liveMax ? alive - config.decayRate : 1;
+      if (alive >= 1 && alive <= 250) {
+        var liveMax = alive < 50 ? config.liveMax : config.liveMax + 3;
+        nextAlive = numAliveNeighbors < config.liveMin || numAliveNeighbors > config.liveMax ? Math.min(alive - config.decayRate, 0.99) : alive + 1;
       } else {
-        nextAlive = numAliveNeighbors < config.deadMin || numAliveNeighbors > config.deadMax ? alive - config.decayRate : 1;
+        nextAlive = numAliveNeighbors < config.deadMin || numAliveNeighbors > config.deadMax ? Math.min(0.99, alive - config.decayRate) : 1;
       }
       // Noise
-      if (y == height - 1 && Math.random() < config.noiseRate || y == height - 2 && Math.random() < config.noiseRate / 2) {
-        nextAlive = 1;
-      }
-      nextAlive = Math.max(0, nextAlive);
-      nextCol.push(nextAlive);
+      nextAlive = config.noiseFn(x, y, width, height, config.noiseRate, nextAlive);
+      nextCol.push(Math.max(0, nextAlive));
     }
     nextGrid.push(nextCol);
   }
@@ -150,13 +208,18 @@ function getNumAliveNeighbors(grid, x, y) {
       if (i == 0 && j == 0) continue;
       if (x + i >= grid.length || y + j >= grid[x].length) continue;
       if (x + i < 0 || y + j < 0) continue;
-      sum += grid[x + i][y + j] == 1 ? 1 : 0;
+      // sum += grid[x + i][y + j] == 1 ? 1 : 0;
+      var val = grid[x + i][y + j];
+      if (val > 1) {
+        val = 1;
+      }
+      sum += val;
     }
   }
   return sum;
 }
 
-ReactDOM.render(React.createElement(App, null), document.getElementById('container'));
+ReactDOM.render(React.createElement(App, { config: greenConfig }), document.getElementById('container'));
 },{"react":10,"react-dom":7}],2:[function(require,module,exports){
 /*
 object-assign
--- js/index.js
+++ js/index.js
@@ -3,7 +3,37 @@ const React = require('react');
 const ReactDOM = require('react-dom');
 const {useState, useEffect, useMemo} = React;
 
-const config = {
+const greenConfig = {
+  gridWidth: 60,
+  gridHeight: 85,
+  msPerTick: 100,
+  liveMin: 2, // if alive, must have at least this many live neighbors to stay alive
+  liveMax: 5, // if alive, must have at most this many live neighbors to stay alive
+  deadMin: 3, // if dead, must have at least this many live neighbors to become alive
+  deadMax: 3, // if dead, must have at most this many live neighbors to become alive
+  noiseRate: 0.2,
+  backgroundColor: 'black',
+  cellColor: '#6B8E23',
+  decayRate: 0.06,
+  onClick: (ev) => {
+    const cellWidth = width / config.gridWidth;
+    const cellHeight = height / config.gridHeight;
+    const gridX = Math.floor(ev.clientX / cellWidth);
+    const gridY = Math.floor(ev.clientY / cellHeight);
+    grid[gridX][gridY] = 1;
+  },
+  noiseFn: (x, y, width, height, noiseRate, nextAlive) => {
+    if (
+      y == 0 && Math.random() < noiseRate
+      // y == 2 && Math.random() < noiseRate / 2
+    ) {
+      return 1;
+    }
+    return nextAlive;
+  },
+};
+
+const redConfig = {
   gridWidth: 50,
   gridHeight: 70,
   msPerTick: 100,
@@ -13,31 +43,60 @@ const config = {
   deadMax: 3, // if dead, must have at most this many live neighbors to become alive
   noiseRate: 0.4,
   backgroundColor: 'black',
+  cellColor: 'red',
   decayRate: 0.01,
+  onClick: (ev) => {
+    const cellWidth = width / config.gridWidth;
+    const cellHeight = height / config.gridHeight;
+    const gridX = Math.floor(ev.clientX / cellWidth);
+    const gridY = Math.floor(ev.clientY / cellHeight);
+    grid[gridX][gridY] = 1;
+  },
+  noiseFn: (x, y, width, height, noiseRate, nextAlive) => {
+    if (
+      y == height - 1 && Math.random() < noiseRate ||
+      y == height - 2 && Math.random() < noiseRate / 2
+    ) {
+      return 1;
+    }
+    return nextAlive;
+  },
 };
 
-function App() {
+
+
+function App(props) {
+  // const {config} = props;
   const {width, height} = document
     .getElementById('container')
     .getBoundingClientRect();
+
+  const [config, setConfig] = useState(() => {
+    return {
+      ...props.config,
+      gridWidth: width > height ? props.config.gridHeight : props.config.gridWidth,
+      gridHeight: width > height ? props.config.gridWidth : props.config.gridHeight,
+    };
+  });
 
   const [grid, setGrid] = useState(initGrid(config.gridWidth, config.gridHeight));
 
   useEffect(() => {
     const interval = setInterval(
       () => {
-        setGrid(grid => advanceGrid(grid));
+        setGrid(grid => advanceGrid(grid, config));
       },
       config.msPerTick,
     );
     return () => clearInterval(interval);
-  }, []);
+  }, [config.gridWidth]);
 
   const cells = [];
   for (let x = 0; x < config.gridWidth; x++) {
     for (let y = 0; y < config.gridHeight; y++) {
       cells.push(<Cell
         key={'cell_' + x + '_' + y}
+        config={config}
         x={x} y={y} alive={grid[x][y]}
       />);
     }
@@ -51,13 +110,7 @@ function App() {
         backgroundColor: config.backgroundColor,
         position: 'relative',
       }}
-      onClick={(ev) => {
-        const cellWidth = width / config.gridWidth;
-        const cellHeight = height / config.gridHeight;
-        const gridX = Math.floor(ev.clientX / cellWidth);
-        const gridY = Math.floor(ev.clientY / cellHeight);
-        grid[gridX][gridY] = 1;
-      }}
+      onClick={config.onClick}
     >
       {cells}
     </div>
@@ -65,33 +118,40 @@ function App() {
 }
 
 function Cell(props) {
-  const {x, y, alive} = props;
+  const {x, y, alive, config} = props;
   const {width, height} = document
     .getElementById('container')
     .getBoundingClientRect();
-  const {gridWidth, gridHeight} = config;
+  const {gridWidth, gridHeight, cellColor} = config;
 
   const cellWidth = width / gridWidth;
   const cellHeight = height / gridHeight;
-  const backgroundColor = 'rgba(255, 0, 0, ' + alive + ')';
+  // const backgroundColor = 'rgba(255, 0, 0, ' + alive + ')';
+  let color = cellColor;
+  if (alive > 50) {
+    color = "#D2691E";
+  }
   return (
     <div
       style={{
         position: 'absolute',
-        backgroundColor,
+        backgroundColor: color,
         top: cellHeight * y,
         left: cellWidth * x,
         width: cellWidth,
         height: cellHeight,
+        opacity: alive,
       }}
     >
     </div>
   );
 }
 
-function advanceGrid(grid) {
-  const width = grid.length;
-  const height = grid[0].length;
+function advanceGrid(grid, config) {
+  // const width = grid.length;
+  // const height = grid[0].length;
+  const width = config.gridWidth;
+  const height = config.gridHeight;
   const nextGrid = [];
   for (let x = 0; x < width; x++) {
     const nextCol = [];
@@ -99,26 +159,24 @@ function advanceGrid(grid) {
       const alive = grid[x][y];
       const numAliveNeighbors = getNumAliveNeighbors(grid, x, y);
       let nextAlive = alive;
-      if (alive == 1) {
+      if (alive >= 1 && alive <= 250) {
+        const liveMax = alive < 50 ? config.liveMax : config.liveMax + 3;
         nextAlive = (
           numAliveNeighbors < config.liveMin ||
           numAliveNeighbors > config.liveMax
-        ) ? alive - config.decayRate : 1;
+        ) ? Math.min(alive - config.decayRate, 0.99) : alive + 1;
       } else {
         nextAlive = (
           numAliveNeighbors < config.deadMin ||
           numAliveNeighbors > config.deadMax
-        ) ? alive - config.decayRate : 1;
+        ) ? Math.min(0.99, alive - config.decayRate) : 1;
       }
       // Noise
-      if (
-        y == height - 1 && Math.random() < config.noiseRate ||
-        y == height - 2 && Math.random() < config.noiseRate / 2
-      ) {
-        nextAlive = 1;
-      }
-      nextAlive = Math.max(0, nextAlive);
-      nextCol.push(nextAlive);
+      nextAlive = config.noiseFn(
+        x, y, width, height,
+        config.noiseRate, nextAlive,
+      );
+      nextCol.push(Math.max(0, nextAlive));
     }
     nextGrid.push(nextCol);
   }
@@ -145,7 +203,12 @@ function getNumAliveNeighbors(grid, x, y) {
       if (i == 0 && j == 0) continue;
       if (x + i >= grid.length || y + j >= grid[x].length) continue;
       if (x + i < 0 || y + j < 0) continue;
-      sum += grid[x + i][y + j] == 1 ? 1 : 0;
+      // sum += grid[x + i][y + j] == 1 ? 1 : 0;
+      let val = grid[x + i][y + j];
+      if (val > 1) {
+        val = 1;
+      }
+      sum += val;
     }
   }
   return sum;
@@ -155,4 +218,7 @@ function getNumAliveNeighbors(grid, x, y) {
 
 
 
-ReactDOM.render(<App />, document.getElementById('container'));
+ReactDOM.render(
+  <App config={greenConfig} />,
+  document.getElementById('container'),
+);
